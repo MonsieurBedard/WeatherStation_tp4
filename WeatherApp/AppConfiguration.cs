1b53df7d33b764e2ac9695b3a84e3aa53e2a654d
Alexandre Bédard <alexandre.bedard.ca@gmail.com>
2020-11-15 20:47:28 -0500

    add class AppConfiguration + appsettings.json

---
+++ WeatherApp/AppConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace WeatherApp
+{
+    static class AppConfiguration
+    {
+        private static IConfiguration configuration;
+
+        public static string GetValue(string str)
+        {
+            if (configuration == null)
+            {
+                initConfig();
+            }
+
+            return $"{configuration[ str ]}";
+        }
+
+        private static void initConfig()
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json");
+            builder.AddUserSecrets<MainWindow>();
+            configuration = builder.Build();
+        }
+    }
+}
